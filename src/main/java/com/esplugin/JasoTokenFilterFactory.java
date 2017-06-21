1944a55c090204579320d7faabf598398be65f4c
jihoon <jihoon@queryjet.com>
2017-06-21 17:28:10 +0900

    수정

--- src/main/java/com/esplugin/JasoTokenFilterFactory.java
+++ src/main/java/com/esplugin/JasoTokenFilterFactory.java
@@ -6,9 +6,6 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 
-/**
- * Created by jhkwon78 on 2017-04-27.
- */
 public class JasoTokenFilterFactory extends AbstractTokenFilterFactory {
     public JasoTokenFilterFactory(IndexSettings indexSettings,
                                  Environment environment,
